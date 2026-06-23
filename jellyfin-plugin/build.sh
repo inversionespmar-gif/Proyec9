f0eec0a3f6a0f428db0adf84bdd3e43d0e59e00d
inversionespmar-gif <inversionespmar@gmail.com>
2026-06-23 13:30:17 -0300

    buid

--- jellyfin-plugin/build.sh
+++ jellyfin-plugin/build.sh
@@ -3,5 +3,5 @@ set -e
 cd "$(dirname "$0")"
 dotnet build -c Release
 mkdir -p ../docker/plugins
-cp bin/Release/net8.0/Jellyfin.Plugin.ScraperBridge.dll ../docker/plugins/
+cp bin/Release/net9.0/Jellyfin.Plugin.ScraperBridge.dll ../docker/plugins/
 echo "Plugin built and copied to docker/plugins/"
